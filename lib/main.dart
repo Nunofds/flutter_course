0d84c59a589412f6bc97e885e69c0cd67b81713d
Nuno Fernandes <n.fernandes.contact@gmail.com>
2024-12-06 00:07:39 +0100

    add fluentui_icons package for icons & create Bottom Nav Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tickets_app/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,20 +8,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final String titleApp = "Tickets App";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tickets App',
+      title: titleApp,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text("Hello"),
-        ),
-      ),
+      home: const BottomNavBar(),
     );
   }
 }
